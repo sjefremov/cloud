1a96c00a2655e2923e4f262ebcc82dd5eb6d354f
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-08-30 20:23:39 +0200

    Added project

---
+++ SendingMail/SendingMail.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System.Threading.Tasks;
+
+namespace SendingMail.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Execute().Wait();
+            Console.ReadLine();
+            
+        }
+
+        static async Task Execute()
+        {
+            // Create the email object first, then add the properties.
+            var mail = new Mail(new Email("riste.poposki@gmail.com"), "Testing the SendGrid Library", new Email(@"orce.petreski@gmail.com"), new Content("text", "Hello World plain text!"));
+
+            var sendGrid = new SendGridAPIClient("");
+            dynamic response = await sendGrid.client.mail.send.post(requestBody: mail.Get());
+        }
+    }
+}
